fc16a8f3f37eeea62ef4a027f08899f2b33ccb0a
E-spacnvrnd <nasdaq80@mail.ru>
2023-06-09 14:30:50 +0300

    feature: add validation for editAvatarPopup

--- src/components/EditAvatarPopup.js
+++ src/components/EditAvatarPopup.js
@@ -1,20 +1,32 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import PopupWithForm from "./PopupWithForm";
 
 
 export default function EditAvatarPopup({ isOpen, onClose, onUpdateAvatar }) {
 
-
   const avatarRef = useRef();
-
+  const [isValid, checkValidity] = useState(false);
+  const [isErrorActive, setErrorActivity] = useState(false);
+  const [message, setMessage] = useState('');
 
   function handleSubmit(e) {
     e.preventDefault();
     onUpdateAvatar({
-      avatar: avatarRef.current.value /* Значение инпута, полученное с помощью рефа */,
+      avatar: avatarRef.current.value /* Значение инпута, полученное с помощью рефа */
     });
     avatarRef.current.value = '';
   }
+
+  function handleValue() {
+    checkValidity(avatarRef.current.validity.valid);
+    setMessage(avatarRef.current.validationMessage);
+    (isValid ? setErrorActivity(false) : setErrorActivity(true))
+  };
+
+  useEffect(() => {
+    setMessage('');
+  }, [isOpen])
+
 
   return (
     <PopupWithForm
@@ -23,9 +35,12 @@ export default function EditAvatarPopup({ isOpen, onClose, onUpdateAvatar }) {
       isOpen={isOpen}
       onClose={onClose}
       buttonText='Сохранить'
-      onSubmit={handleSubmit}>
+      onSubmit={handleSubmit}
+      isValid={isValid}
+    >
       {<fieldset className="form__field form__field_profile-avatar">
         <input className="form__input form__input_el_avatar"
+          onChange={handleValue}
           ref={avatarRef}
           id="avatar-input"
           name="avatar"
@@ -36,7 +51,7 @@ export default function EditAvatarPopup({ isOpen, onClose, onUpdateAvatar }) {
           minLength="2"
           maxLength="200"
           required />
-        <span className="avatar-input-error form__input-error"></span>
+        <span className={`avatar-input-error form__input-error ${isErrorActive ? 'form__input-error_active' : ''}`}>{message}</span>
       </fieldset>}
     </PopupWithForm>
   )
